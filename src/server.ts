8b8c3fe5b873f1a55036694d6a0e949a516b86a5
Rafael Freitas <rafaelborges1985@hotmail.com>
2021-02-25 08:39:18 -0300

    Finish database config

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,19 @@
+import "reflect-metadata";
+import "./database"
 import express from "express";
 
 const app = express()
+
+app.get("/", (req, res) => {
+  return res.json({
+    message: "RAFAEL"
+  })
+});
+
+app.post("/", (req, res) => {
+  return res.json({
+    message: "Data saved successfully"
+  })
+})
 
 app.listen(4000, () => console.log("Server is running on port: 4000"));
